80b0fd617af35e23cd9f3ca69854966d5bfaac44
Jader Alves <DevJaderAlves@gmail.com>
2025-04-25 14:29:36 -0300

    Corrigido index.js e limpo rotas duplicadas

--- index.js
+++ index.js
@@ -4,37 +4,17 @@ const clienteRoutes = require('./routes/clientes');
 
 const app = express();
 
-app.use('/clientes', clienteRoutes);
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/clientes', async (req, res) => {
-  try {
-    const clientes = await prisma.cliente.findMany();
-    res.json(clientes);
-  } catch (error) {
-    console.error("Erro detalhado:", error); // exibe erro no console do Render
-    res.status(500).json({ error: 'Erro ao listar clientes' });
-  }
-});
+// Aplicando as rotas corretas
+app.use('/clientes', clienteRoutes);
 
-app.post('/clientes', async (req, res) => {
-  try {
-    const { nome, email, telefone, status } = req.body;
-    const novoCliente = await prisma.cliente.create({
-      data: { nome, email, telefone, status }
-    });
-    res.json(novoCliente);
-  } catch (error) {
-    console.error(error); // ajuda no debug
-    res.status(500).json({ error: 'Erro ao criar cliente' });
-  }
-});
-
+// Sobe o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 
