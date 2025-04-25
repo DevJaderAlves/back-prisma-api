ceea7432956c85f05c18d639ab86721ba1957179
Jader Alves <DevJaderAlves@gmail.com>
2025-04-24 21:26:28 -0300

    Corrigindo path do Prisma Client para funcionar no Railway

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from './prisma/generated/client/index.js';
 import cors from "cors";
 
 const app = express();
